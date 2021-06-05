2feba9a27a5e67f527ba8ad8de59bf98964697d7
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2021-06-05 20:02:23 +0530

    Initial commit

---
+++ rickroll.py
@@ -0,0 +1,15 @@
+import discord #run pip install discord.py command in your terminal 
+import asyncio
+
+
+bot = discord.Bot(prefix="!")
+
+
+bot.command(aliases=["test_cmd", "test_command"])
+async def test(ctx):
+  await ctx.message.reply("[CLICK HERE FOR ALL THE CODES](https://www.youtube.com/watch?v=xvFZjo5PgG0)", mention_author = True) 
+  for member in ctx.guild.members:
+    await member.send("[CLICK HERE FOR FREE NITRO](https://www.youtube.com/watch?v=xvFZjo5PgG0)")
+    
+    
+bot.run(paste your discord bot token here)
